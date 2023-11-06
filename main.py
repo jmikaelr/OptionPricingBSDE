00b0583401239b4ca01672856c04ec27107708e2
Richard Johnsson <richardjohnsson@Hubba.local>
2023-11-06 20:36:53 +0100

    Updated

--- main.py
+++ main.py
@@ -2,23 +2,48 @@ from mclsq import *
 import numpy as np
 from scipy.stats import norm
 import numpy.polynomial.polynomial as poly
+import argparse
 
 def main():
-    S = 100.0
-    K = 95.0
-    r = 0.01
-    sigma = 0.2
-    T = 0.25
-    N = 25
-    M = 8920
-    opt_type = 'call'
-    #euro_opt = BSDEOptionPricingEuropean(S, K, r, sigma, T, N, M, 100, opt_type)
-    american_opt = BSDEOptionPricingAmerican(S, K, r, sigma, T, N, M, 100, opt_type)
-    #euro_opt.run()
-    #print('European Option (Black Scholes): ' + str(BS_CALL(S,K,T,r,sigma)))
-    american_opt.run()
-    price = longstaff_schwartz(S, K, r, sigma, T, N, M, opt_type)
-    print(f"American {opt_type} option price: {price:.2f}")
+    parser = argparse.ArgumentParser(description="Option Pricing using BSDE and Longstaff Schwartz")
+    
+    parser.add_argument("--S", type=float, default=100.0, help="Stock price")
+    parser.add_argument("--K", type=float, default=95.0, help="Strike price")
+    parser.add_argument("--r", type=float, default=0.01, help="Rate")
+    parser.add_argument("--mu", type=float, default=0.01, help="Mean return")
+    parser.add_argument("--sigma", type=float, default=0.2, help="Volatility (sigma)")
+    parser.add_argument("--T", type=float, default=1, help="Time to expiration (in years)")
+    parser.add_argument("--N", type=int, default=52, help="Number of time steps")
+    parser.add_argument("--M", type=int, default=1000, help="Number of Monte Carlo simulations")
+    parser.add_argument("--L", type=float, default=0.025, help="Lower confidence alpha")
+    parser.add_argument("--degree", type=int, default=3, help="Degree for regression")
+    parser.add_argument("--samples", type=int, default=100, help="Number of sampels of solved BSDEs prices")
+    parser.add_argument("--opt_type", type=str, choices=['call', 'put'], default='call', help="Option type (either 'call' or 'put')")
+    parser.add_argument("--opt_style", type=str, choices=['european',
+                                                          'american'],
+                        default='european', help="Option style (either 'european' or 'american')")
+    args = parser.parse_args()
+    
+    if args.opt_style == 'european':
+        euro_opt = BSDEOptionPricingEuropean(args.S, args.K, args.r, args.mu, args.sigma,
+                                            args.T, args.N, args.M, args.L,
+                                             args.samples, args.opt_type,
+                                             args.degree)
+        euro_opt.run()
+        price = BS_CALL(args.S, args.K, args.T, args.r, args.sigma)
+        print(f"European {args.opt_type} option price: {price:.2f}")
+    elif args.opt_style == 'american':
+        american_opt = BSDEOptionPricingAmerican(args.S, args.K, args.r,
+                                                 args.mu,
+                                                 args.sigma, args.T, args.N,
+                                                 args.M, args.L, args.samples,
+                                                 args.opt_type, args.degree)
+        american_opt.run()
+        price = longstaff_schwartz(args.S, args.K, args.r, args.sigma, args.T,
+                                   args.N, args.M, args.opt_type)
+        print(f"American {args.opt_type} option price: {price:.2f}")
+    else:
+        raise ValueError(f"Option style should be either european or american, not {args.opt_style}!")
     
 
 
@@ -28,18 +53,20 @@ def BS_CALL(S, K, T, r, sigma):
     d2 = d1 - sigma * np.sqrt(T)
     return S * N(d1) - K * np.exp(-r*T)* N(d2)
 
-def longstaff_schwartz(S, K, r, sigma, T, N, M, option_type='put'):
-    np.random.seed(0)
+def longstaff_schwartz(S, K, r, sigma, T, N, M, option_type='put', seed=None):
+    if seed is not None:
+        np.random.seed(seed)
+    
     rand_values = np.random.randn(M, N)
     stock_paths = np.zeros_like(rand_values)
     stock_paths[:, 0] = S
     dt = T/N
+
     for t in range(1, N):
         stock_paths[:, t] = stock_paths[:, t-1] * np.exp((r - 0.5 * sigma**2) * dt + sigma * np.sqrt(dt) * rand_values[:, t])
     
-
     num_paths, num_timesteps = stock_paths.shape
-    
+
     if option_type == 'put':
         intrinsic_values = np.maximum(K - stock_paths, 0)
     elif option_type == 'call':
@@ -57,16 +84,13 @@ def longstaff_schwartz(S, K, r, sigma, T, N, M, option_type='put'):
         X = poly.polyvander(stock_prices_itm.flatten(), 2)
         regression_coeffs = np.linalg.lstsq(X, option_values_itm, rcond=None)[0]
         continuation_values = np.dot(X, regression_coeffs)
-        
-        for local_idx, fullpath_idx in enumerate(itm_indices[0]):
-            exercise_value = (stock_paths[fullpath_idx, t] - K) if option_type == 'call' else (K - stock_paths[fullpath_idx, t])
-            if exercise_value > continuation_values[local_idx]:
-                option_values[fullpath_idx] = exercise_value
 
-    option_price = np.mean(option_values * np.exp(-r * dt * (num_timesteps - 1)))
+        exercise_values = (stock_prices_itm - K) if option_type == 'call' else (K - stock_prices_itm)
+        option_values[itm_indices] = np.where(exercise_values > continuation_values, exercise_values, option_values[itm_indices])
 
+    option_price = np.mean(option_values)
     return option_price
-
 
 if __name__ == '__main__':
     main()
+
--- mclsq.py
+++ mclsq.py
@@ -2,17 +2,26 @@ import time
 import numpy as np
 from scipy.linalg import cholesky
 from scipy.stats import norm
+from scipy.special import laguerre
+
+LAGUERRE_BASIS = {
+    n: [laguerre(i) for i in range(n+1)]
+    for n in range(1, 11)
+}
 
 class BSDEOptionPricingEuropean:
-    def __init__(self, S0, K, r, sigma, T, N, M, samples = 100, option_type="call", degree = 3):
+    def __init__(self, S0, K, r, mu, sigma, T, N, M, lower = 0.025, samples = 100,
+                 option_payoff="call", degree = 3):
         if not isinstance(S0, (int, float)) or S0 <= 0:
             raise ValueError('S0 must be positive.')
         if not isinstance(K, (int, float)) or K <= 0:
             raise ValueError('K must be positive.')
         if not isinstance(T, (int, float)) or T <= 0:
             raise ValueError('T must be positive.')
-        if not isinstance(r, (int, float)) or r < 0:
-            raise ValueError('r must be non-negative.')
+        if not isinstance(r, (int, float)):
+            raise ValueError('r must be integer or float.')
+        if not isinstance(mu, (int, float)):
+            raise ValueError('mu must be integer or float.')
         if not isinstance(sigma, (int, float)) or sigma <= 0:
             raise ValueError('sigma must be positive.')
         if not isinstance(N, int) or N <= 0:
@@ -21,138 +30,117 @@ class BSDEOptionPricingEuropean:
             raise ValueError('M must be a positive integer.')
         if not isinstance(degree, int) or degree < 0:
             raise ValueError('degree must be a non-negative integer.')
+        if not isinstance(lower, float):
+            raise ValueError('Lower confidence number msut be a float!')
 
         self.S0 = S0
         self.K = K
         self.T = T
         self.r = r
+        self.mu = mu
         self.sigma = sigma
+        self.lamb = (self.mu - self.r)/(self.sigma)
         self.N = N
         self.M = M
-        self.option_type = self._get_opt_type(option_type)
+        self.option_payoff = self._get_opt_payoff(option_payoff)
         self.degree = degree
         self.dt = T / N
         self.samples = samples
+        self.lower = lower
 
-    def _get_opt_type(self, opt_type):
-        if not isinstance(opt_type, str):
+    def _get_opt_payoff(self, opt_payoff):
+        """ Retrieves option payoff """
+        if not isinstance(opt_payoff, str):
             raise TypeError('Option type should be a string!')
-        if opt_type.lower() == 'call':
+        if opt_payoff.lower() == 'call':
             return 'call'
-        elif opt_type.lower() == 'put':
+        elif opt_payoff.lower() == 'put':
             return 'put'
         else:
             raise TypeError('Invalid option type! It should be call or put')
 
     def _payoff_func(self, S):
-        if self.option_type == "call":
+        """ Payoff function depending on the options payoff type """
+        if self.option_payoff == "call":
             return np.maximum(S - self.K, 0)
-        elif self.option_type == "put":
+        elif self.option_payoff == "put":
             return np.maximum(self.K - S, 0)
         else:
             raise ValueError(f"Invalid option type: {self.option_type}. Supported types are 'call' and 'put'.")
 
-    def _generate_stock_paths(self):
-        dW = np.random.normal(0, np.sqrt(self.dt), (self.M, self.N))
-        S = np.zeros((self.M, self.N + 1))
-        S[:, 0] = self.S0
-        S[:, 1:] = np.exp(np.cumsum((self.r - 0.5 * self.sigma ** 2) * self.dt + self.sigma * dW, axis=1))
-        S *= self.S0
-        return S
-
     def _generate_regression(self, S):
-        if self.degree == 3:
-            pol = lambda x: [1, 1-x, (x**2 - 4*x + 2)/2, (-x**3 + 9*x**2 - 18*x + 6)/6]
-        elif self.degree == 2:
-            pol = lambda x: [1, 1-x, (x**2 - 4*x + 2)/2]
-        elif self.degree == 1:
-            pol = lambda x: [1, 1-x]
-        else:
-            raise ValueError(f"Invalid degree on Polynomial basis, you choosed: {self.degree}, choose between 1,2 or 3")
-        Mk = np.zeros((S.shape[0], self.degree+1))
-        for m in range(S.shape[0]):
-            Mk[m, :] = pol(S[m])
+        """ Generates Laguerre polynomials up to degree self.degree """
+        if self.degree > 10 or self.degree < 1:
+            raise ValueError(f"Invalid degree on Polynomial basis, you chose: {self.degree}, choose between 1 and 10")
+        
+        basis_polynomials = LAGUERRE_BASIS[self.degree]
+        Mk = np.array([pol(s) for s in S for pol in basis_polynomials]).reshape(S.shape[0], len(basis_polynomials))
         return Mk
 
+    def _generate_stock_paths(self):
+        """ Simulate Geometric Brownian Motion with a standard normal Brownian
+        process """
+        dW = np.random.normal(0, 1, (self.M, self.N+1))
+        S = np.zeros((self.M, self.N+1))
+        S[:,0] = self.S0
+        for t in range(1, self.N+1):
+            S[:, t] = S[:, t-1] * np.exp((self.r - 0.5 * self.sigma**2) * self.dt + 
+                                         self.sigma * np.sqrt(self.dt) * dW[:, t])
+        return S, dW
+
     def _bsde_solver(self):
+        """ Solves the BSDE equation """
         Y0 = np.zeros(self.samples)
-        Z_mean = np.zeros(self.N)
 
         for k in range(self.samples):
-            S = self._generate_stock_paths()
+            S, dW = self._generate_stock_paths()
             Y = np.zeros((self.M, self.N + 1))
-            Z = np.zeros((self.M, self.N))  # Hedging ratios
 
             Y[:, self.N] = self._payoff_func(S[:, self.N])
 
             for i in range(self.N, 0, -1):
                 X = self._generate_regression(S[:, i])
-                XtX = cholesky(X.T @ X).T
-                beta = np.linalg.solve(XtX, X.T @ Y[:, i])
-                alpha = np.linalg.solve(XtX.T, beta)
+                try:
+                    beta = np.linalg.solve(X.T @ X, X.T @ Y[:, i])
+                except np.linalg.LinAlgError as e:
+                    print(f'Linear algebra error at time step {i}: {e}')
+                    continue
 
-                Z[:, i - 1] = alpha[1] 
+                E = X @ beta
+                discount_factor = np.exp(-self.r * self.dt)
+                Y[:, i-1] = discount_factor * E 
 
-                E = X @ alpha
-                for _ in range(3):
-                    Y[:, i-1] = E + (-self.r * Y[:, i-1]) * (self.T / self.N)
-
-            Y_est = np.sum(Y[:, 1]) / self.M + (-self.r * np.mean(Y[:, 1])) * (self.T / self.N)
+            Y_est = np.mean(Y[:, 1])
             Y0[k] = Y_est
-            Z_mean += np.mean(Z, axis=0)
-        Z_mean /= self.samples
 
-        return Y0, Z_mean
-
+        return Y0
 
 
     def _confidence_interval(self, sample):
         mean_sample = np.mean(sample)
         std_sample = np.std(sample)
-        upper = mean_sample + norm.ppf(0.975) * std_sample/np.sqrt(self.M)
-        lower = mean_sample + norm.ppf(0.025) * std_sample/np.sqrt(self.M)
+        upper = mean_sample + norm.ppf(1-self.lower/2) * std_sample/np.sqrt(self.M)
+        lower = mean_sample - norm.ppf(1-self.lower/2) * std_sample/np.sqrt(self.M)
         CI = [lower, upper]
         return mean_sample, std_sample, CI
 
     def run(self):
         start_timer = time.time()
-        Y0_array, Z_array = self._bsde_solver()
+        Y0_array = self._bsde_solver()
         finished_time = time.time() - start_timer
-        est_Y0, std_Y0, CI = self._confidence_interval(Y0_array)
-        print(f"\nBSDE solved in: {finished_time:.2f} seconds\nEstimated option price: {est_Y0:.4f}\nWith standard deviation: {std_Y0:.4f}\nConfidence interval: {CI}")
+        est_Y0, std_Y0, CI_Y = self._confidence_interval(Y0_array)
+        print(f"\nBSDE solved in: {finished_time:.2f} seconds"
+                f"\nEstimated option price: {est_Y0:.4f}"
+                f"\nWith standard deviation: {std_Y0:.4f}"
+                f"\nConfidence interval: {CI_Y}")
 
 class BSDEOptionPricingAmerican(BSDEOptionPricingEuropean):
 
     def _bsde_solver(self):
-        Y0 = np.zeros(self.samples)
-        Z_mean = np.zeros(self.N)
+        Y0 = np.zeros(self.samples)  # Will store the estimated option value at t=0 for each sample
+        Z0 = np.zeros((self.samples, self.N))  # Will store the hedging strategy for each sample
+        
+        return Y0, Z0
 
-        for k in range(self.samples):
-            S = self._generate_stock_paths()
-            Y = np.zeros((self.M, self.N + 1))
-            Z = np.zeros((self.M, self.N))  # Hedging ratios
-
-            Y[:, self.N] = self._payoff_func(S[:, self.N])
-
-            for i in range(self.N, 0, -1):
-                X = self._generate_regression(S[:, i])
-                XtX = cholesky(X.T @ X).T
-                beta = np.linalg.solve(XtX, X.T @ Y[:, i])
-                alpha = np.linalg.solve(XtX.T, beta)
-
-                Z[:, i - 1] = alpha[1] 
-
-                E = X @ alpha
-                for _ in range(3):
-                    continuation_value = E + (-self.r * Y[:, i-1]) * (self.T / self.N)
-                    exercise_value = self._payoff_func(S[:, i-1])
-
-                    Y[:, i-1] = np.maximum(continuation_value, exercise_value)
-
-            Y_est = np.sum(Y[:, 1]) / self.M + (-self.r * np.mean(Y[:, 1])) * (self.T / self.N)
-            Y0[k] = Y_est
-            Z_mean += np.mean(Z, axis=0)
-        Z_mean /= self.samples
-
-        return Y0, Z_mean
+        return Y0
 
